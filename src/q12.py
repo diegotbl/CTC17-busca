c8240d3913eb1b719835c70abf69c77347fe1b25
vidal <jose.otavio.vidal@gmailcom>
2019-09-15 14:40:49 -0300

    Agora eh a final mesmo ate pq se tiver errado, nao mando outra

--- src/q12.py
+++ src/q12.py
@@ -1,23 +1,27 @@
 from random import uniform, randint
 from math import exp, pow
 
+
 def quad(x, y):
-    return (pow(x,2) + pow(y,2))
+    return pow(x,2) + pow(y,2)
+
 
 def J(neightor):
     x = neightor[0]
     y = neightor[1]
     return 4*exp(-quad(x, y)) + exp(-quad(x-5, y-5)) + exp(-quad(x-5, y+5)) + exp(-quad(x+5, y-5)) + exp(-quad(x+5, y+5))
 
+
 def temperature_schedule(i):
     T_0 = 5.0
     Beta = 0.5
     return T_0/(1+Beta*i)
 
+
 def random_neighbor(theta):
     k = uniform(-2.5, 2.5)
     l = uniform(-2.5, 2.5)
-    return (theta[0]+k, theta[1]+l)
+    return theta[0]+k, theta[1]+l
 
 
 def simulated_annealing(theta):
@@ -27,7 +31,6 @@ def simulated_annealing(theta):
         T = temperature_schedule(i)
         if T <= 0.00001:
             if J(maximum) <= J(theta):
-
                 return theta
             return maximum
         neighbor = random_neighbor(theta)
@@ -39,6 +42,7 @@ def simulated_annealing(theta):
             if r <= exp(deltaE/T):
                 theta = neighbor
         i = i+1
+
 
 if __name__ == "__main__":
     min = 0
